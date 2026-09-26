9b454082fba1b90b8f2f64433b96a4310544ff08
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-09-26 09:21:42 +0700

    chore: show v0.5 in editor

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -309,7 +309,7 @@ export function Topbar({ onOpenCode }: { onOpenCode: () => void }) {
         <div className="brand">
           <span className="brand-mark">C</span>
           <span>CodeCanvas</span>
-          <small>v0.4</small>
+          <small>v0.5</small>
         </div>
 
         <nav className="app-menu-bar">
